f8250e50ba4e9a4669b0588ac4cc45ffecddd61f
ramesh12331 <mamidiramesh503@gmail.com>
2025-05-26 13:29:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import NavBar from './NavBar'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Body from './Body'
+import Login from './Login'
+import Profile from './Profile'
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element = {<Body/>}>
+            <Route path="/login" element = {<Login/>}/>
+            <Route path="/profile" element = {<Profile/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
