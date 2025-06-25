30282dabb1ea2fc6a2eeb3957e1f1f313fcb1423
ramesh12331 <mamidiramesh503@gmail.com>
2025-06-25 11:02:32 +0530

    Hello

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import NavBar from '../components/NavBar'
-import { Outlet } from 'react-router-dom'
-import Footer from '../components/Footer'
+import React from "react";
+import NavBar from "../components/NavBar";
+import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer";
 
 const Body = () => {
   return (
     <div>
-      <NavBar/>
-      <Outlet/>
-      <Footer/>
+      <NavBar />
+      <Outlet />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Body
+export default Body;
